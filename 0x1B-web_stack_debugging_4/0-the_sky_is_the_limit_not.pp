a8b1896e4f796b99cced7b8acc40f5c0bf984635
Eyob Getachew <99553287+Eyob19th@users.noreply.github.com>
2023-10-19 21:26:57 +0300

    Delete 0x1B-web_stack_debugging_4 directory

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++
@@ -1,5 +0,0 @@
-# Sky is the limit, let's bring that limit higher
-exec {'/etc/default/nginx':
-command  => 'sed -i "s/15/500/g" /etc/default/nginx; sudo service nginx restart',
-provider => shell,
-}
